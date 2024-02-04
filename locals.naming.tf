d3eb92414d33e5b473adc8ec44d6ea98ae0050c4
John Spinella <jrspinella@users.noreply.github.com>
2024-02-04 16:08:01 -0500

    Fix scaffolding

--- locals.naming.tf
+++ locals.naming.tf
@@ -3,7 +3,7 @@ locals {
   name_prefix = lower(var.name_prefix)
   name_suffix = lower(var.name_suffix)
 
-  resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, module.mod_scaffold_rg.*.resource_group_name, [""]), 0)
-  location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, module.mod_scaffold_rg.*.resource_group_location, [""]), 0)
+  resource_group_name = element(coalescelist(data.azurerm_resource_group.rg.*.name, module.mod_scaffold_rg.*.resource_group_name, [""]), 0)
+  location            = element(coalescelist(data.azurerm_resource_group.rg.*.location, module.mod_scaffold_rg.*.resource_group_location, [""]), 0)
   cosmosdb_name       = coalesce(var.custom_cosmosdb_name, data.azurenoopsutils_resource_name.example_custom_name.result)
 }
